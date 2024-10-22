2f1ed9929e951cd7558a8f4f7aa515b9f2d73ca4
Max Schaible <max.schaible@mailbox.tu-dresden.de>
2024-10-22 13:32:44 +0200

    info log

--- python/abstract_finder/find_abstract_app.py
+++ python/abstract_finder/find_abstract_app.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.pg_model import get_engine, DNBRecord, get_session
 
 # Set up logging
-logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 # Load environment variables
