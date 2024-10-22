77ca84b9dbfec1b1bbe8d1749ba59964de77024c
Max Schaible <max.schaible@mailbox.tu-dresden.de>
2024-10-22 15:33:44 +0200

    pip package and chmod startup

--- python/converter/startup.sh
+++ python/converter/startup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 pip install -r requirements.txt
 # Install packages from google index
-pip install -f https://storage.googleapis.com/libtpu-releases/index.html
+pip install torch~=2.4.0 torch_xla[tpu]~=2.4.0 torchvision -f https://storage.googleapis.com/libtpu-releases/index.html
 # set runtime
 export PJRT_DEVICE=TPU
 
 python convert.py
+
